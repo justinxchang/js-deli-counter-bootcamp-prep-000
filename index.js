51377c8da4f962b6017d13a2941eefd19182ff81
Justin Chang <justinxchang@hotmail.com>
2018-08-20 22:29:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function takeANumber(currentLine, name){
   return (`Welcome, ${name}. You are number ${currentLine.length} in line.`)
 }
 
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length > 1){
+    
+  }
+}
