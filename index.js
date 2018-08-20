ca166437281a7c587a7fd83c9e186de3548b9d25
Justin Chang <justinxchang@hotmail.com>
2018-08-20 22:27:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 
 
 function takeANumber(currentLine, name){
-  for(let i = 0; i<currentLine.length; i++){
-    currentLine.push(name);
-  }
+  currentLine.push(name);
   return (`Welcome, ${name}. You are number ${currentLine.length} in line.`)
 }
