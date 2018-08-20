2dc3bdc29774a11805b178b70ffaf535a87b6336
Justin Chang <justinxchang@hotmail.com>
2018-08-20 22:40:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(katzDeliLine){
 function currentLine(arr){
   if(arr.length > 1){
     for(let i=0; i<arr.length; i++){
-      return `${i}. ${arr[i]}`;
+      return `${i+1}. ${arr[i]}`;
      }
     } else {
       return "The line is currently empty."
